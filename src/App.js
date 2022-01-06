f78ef0b5a3a765d4329e852724336207bfb3ba86
Alex <navia09alex64@gmail.com>
2022-01-06 01:00:14 -0600

    Started Task Tracker App - created Button / Tasks / Task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+  }
+])
   return (
-    <div className="App">
-      <h1> Test </h1>
+    <div className="container">
+      <h1> Hello From React </h1>
+      <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
+
 
 export default App;
